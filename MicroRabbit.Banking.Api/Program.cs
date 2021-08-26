9eda375fe760b13bdf2ec19efa0cc04b9e61b212
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-08-27 00:44:44 +0100

    Remove unused using statements

--- MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit.Banking.Api/Program.cs
@@ -1,14 +1,10 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Seeder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace MicroRabbit.Banking.Api
 {
--- MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,6 +1,5 @@
 using MicroRabbit.Banking.Application.Interfaces;
 using MicroRabbit.Banking.Application.Services;
-using MicroRabbit.Banking.Data.Context;
 using MicroRabbit.Banking.Data.Repository;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Domain.Core.Bus;
