08626bfcbd00b0516c45b9f3ae100d6e315c34e0
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-08-26 14:38:49 +0100

    Create AccountService for Banking.Application

---
+++ MicroRabbit.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Domain.Models;
+
+namespace MicroRabbit.Banking.Application.Interfaces
+{
+    public interface IAccountService
+    {
+        Task<IReadOnlyList<Account>> GetAccounts();
+    }
+}
