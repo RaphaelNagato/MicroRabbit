a58d57a239f2272b10e5235f00178720fd0a8692
Raphael Okeibunor <raphaelnagato19@gmail.com>
2021-08-27 02:10:45 +0100

    Create GetAccounts Endpoint

---
+++ MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+
+        private readonly IAccountService _accountService;
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _accountService.GetAccounts());
+        }
+    }
+}
